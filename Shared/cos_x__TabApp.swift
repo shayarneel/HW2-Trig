b9bf47f91250c50d3779befdd255b9e9f0b7d1a2
Jeff Terry <terryj@iit.edu>
2021-01-23 21:50:02 -0600

    Initial Commit

---
+++ Shared/cos_x__TabApp.swift
@@ -0,0 +1,17 @@
+//
+//  cos_x__TabApp.swift
+//  Shared
+//
+//  Created by Jeff Terry on 1/23/21.
+//
+
+import SwiftUI
+
+@main
+struct cos_x__TabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
